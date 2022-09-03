698ea12468c4226dbf610cc06e9bb2d5fa5a99d2
Felcia <felcialin@gmail.com>
2022-09-03 15:09:30 +1000

    Modify index, add images to menu

--- app.py
+++ app.py
@@ -36,13 +36,80 @@ def after_request(response):
 
 @app.route("/")
 def index():
+    products = db.execute("Select * from products;")
+    print(products)
+    if products is not None:
+        return render_template("index.html", data = products)
     return apology("test")
 
-login_required()
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
+
+    if request.method == "POST":
+        """Register user"""
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        if len(rows) != 0:
+            return apology("username already exists", 400)
+
+        if not request.form.get("password") or not request.form.get("confirmation"):
+            return apology("must provide password", 400)
+
+        if request.form.get("password") != request.form.get("confirmation"):
+            return apology("password not matched", 400)
+
+        pwd = generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8)
+
+        insertQuery = "INSERT INTO users(username, hash) VALUES('" + request.form.get("username") + "', '" + pwd + "') "
+        db.execute(insertQuery)
+
+        return redirect("/login")
     return render_template("register.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login")
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?",
+                          request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
---
+++ static/index.js
@@ -0,0 +1,5 @@
+function addToCart(event){
+    alert("add to cart clicked")
+    console.log(event)
+    document.getElementById("shoppingCart").style.display = "block";
+}
